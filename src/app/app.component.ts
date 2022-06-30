1cc712f48375e96626cf906cb57748e4baa675a7
Mathias Van Den Broek <Mathias.VandenBroek@dematic.com>
2022-06-30 15:15:54 +0200

    Add dynamic config file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';
+import {Observable} from 'rxjs';
+
+interface Config {
+  name: string;
+  showButton: boolean;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +15,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'workshop-devops-fe-angular';
+  name$: Observable<string>;
+  showButton$: Observable<boolean>;
+
+  constructor(http: HttpClient) {
+    const config$: Observable<Config> = http.get<Config>('/config/config.json');
+    this.name$ = config$.pipe(map(c => c.name));
+    this.showButton$ = config$.pipe(map(c => c.showButton));
+  }
+
+  handleClick(): void {
+    alert('ANGULAR RULEZ!');
+  }
 }
